430863a0ff3252518505d7956084a85cf64156bb
Roosevelt Franklin <roosevelt_franklin@hotmail.com>
2020-11-06 20:48:40 -0300

    INclude SASS in project

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,21 +1,34 @@
 <template>
-    <section class="section one" id="s1">
-        <div class="top-deco deco-1">
-            <span class="icon"><i class="fas fa-code"></i></span>
-        </div>
+    <section class="section" id="s1">
         <div class="center">
             <img src="assets/images/logo.png" alt="Logo RCode" />
-            <p class="lead">Olá, me chamo Roosevelt Franklin e desenvolvo sites e aplicativos.<br>Clique no menu abaixo e conheça melhor o meu trabalho :)</p>
+            <p class="big">Olá, me chamo Roosevelt Franklin e desenvolvo sites e aplicativos.<br>Clique no menu abaixo e conheça melhor o meu trabalho :)</p>
             <!-- <a href="#">English Version</a> -->
         </div>
     </section>
 </template>
 
-<script>
+<style scoped lang="scss">
+img{
+    margin-bottom:20px;
+}
+@media only screen and (max-width: 768px) {
+    img{width:240px;}
+}
 
-</script>
+@media only screen and (min-width: 768px) {
+    img{width:320px;}
+}
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+@media only screen and (min-width: 992px) {
+    img{width:400px;}
+}
 
+@media only screen and (min-width: 1440px) {
+    img{width:540px;}
+}
+
+@media only screen and (min-width: 1920px) {
+    img{width:720px;}
+}
 </style>
--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -16,7 +16,7 @@
 
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-
+<style lang="scss" scoped>
+  @import "../assets/scss/_variables.scss";
+  @import "../assets/scss/_menu.scss";
 </style>
